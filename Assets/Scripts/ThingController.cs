fc769c8421e9eb22a6deb6bc5a9310051d068391
Hernan Quintana <hernan@jsatch.com>
2022-10-07 17:47:56 -0500

    Enemigo camina automaticamente

--- Assets/Scripts/ThingController.cs
+++ Assets/Scripts/ThingController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class ThingController : MonoBehaviour
@@ -9,38 +10,88 @@ public class ThingController : MonoBehaviour
     private float speed;
     [SerializeField]
     private float maxHealth;
+    [SerializeField]
+    private float lookDistance = 10;
 
     private Rigidbody2D mRb;
     private Animator mAnimator;
+    private Slider mSlider;
+    private Transform mCanvas;
+    private Transform mRaycastPoint;
     private float mHealth;
+
+    private bool isAttacking = false;
 
     private void Start()
     {
         mRb = GetComponent<Rigidbody2D>();
         mAnimator = GetComponent<Animator>();
+        mSlider = transform.Find(
+            "Canvas"
+        ).Find(
+            "HealthBar"
+        ).Find(
+            "Border"
+        ).GetComponent<Slider>();
+
+        mCanvas = transform.Find("Canvas");
+        mRaycastPoint = transform.Find("RaycastPoint");
+
         mHealth = maxHealth;
+        mSlider.maxValue = maxHealth;
         
     }
 
     private void Update()
     {
-        mRb.velocity = new Vector2(-speed, mRb.velocity.y);
-
+        if (isAttacking)
+        {
+            mRb.velocity = new Vector2(-speed, mRb.velocity.y);
+        }else
+        {
+            mRb.velocity = new Vector2(0f, mRb.velocity.y);
+        }
+        
+        Look();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            mHealth -= 25f;
-            Debug.Log($"Enemy Health: {mHealth}");
-            if (mHealth <= 0)
-            {
-                // Morir
-                mRb.velocity = Vector2.zero;
-                mAnimator.SetTrigger("Die");
-            }
+            Hurt();
         }
         
+    }
+
+    private void Look()
+    {
+        RaycastHit2D hit =  Physics2D.Raycast(
+            mRaycastPoint.position,
+            Vector3.left,
+            lookDistance);
+
+        if (hit)
+        {
+            isAttacking = true;
+        }else
+        {
+            isAttacking = false;
+        }
+    }
+
+    private void Hurt()
+    {
+        mHealth -= 25f;
+        // Disminuir la vida en el slider
+        mSlider.value = mHealth;
+
+        if (mHealth <= 0f)
+        {
+            // Morir
+            mCanvas.gameObject.SetActive(false);
+            mRb.velocity = Vector2.zero;
+            mAnimator.SetTrigger("Die");
+        }
     }
 }
